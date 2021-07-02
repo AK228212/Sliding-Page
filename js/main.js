610bdec8261239124b0e68b467dce50609b37029
ABhishekkumar <practice@something.com>
2021-07-02 22:18:50 +0530

    Added JS for full page animation and add move upside button

--- js/main.js
+++ js/main.js
@@ -7,3 +7,43 @@ toggleButton.addEventListener('click', () => {
 
 
 const carouselSlide = $('.main-container');
+
+$(function() {
+  $.scrollify({
+		section:".panel",
+    scrollbars:false,
+    before:function(i,panels) {
+
+      var ref = panels[i].attr("data-section-name");
+     
+      $(".side-scroller .active").removeClass("active");
+      console.log(ref);
+      $(".side-scroller").find("a[href=\"#" + ref + "\"]").addClass("active");
+    },
+    afterRender:function() {
+      var pagination = "<ul class=\"side-scroller\">";
+      var activeClass = "";
+      $(".panel").each(function(i) {
+        activeClass = "";
+        if(i===$.scrollify.currentIndex()) {
+          activeClass = "active";
+        }
+        pagination += "<li><a class=\"" + activeClass + "\" href=\"#" + $(this).attr("data-section-name") + "\"><span class=\"hover-text\">" + $(this).attr("data-section-name").charAt(0).toUpperCase() + $(this).attr("data-section-name").slice(1) + "</span></a></li>";
+      });
+
+      pagination += "</ul>";
+
+      $(".home").append(pagination);
+      /*
+
+      Tip: The two click events below are the same:
+
+      $(".pagination a").on("click",function() {
+        $.scrollify.move($(this).attr("href"));
+      });
+
+      */
+      $(".side-scroller a").on("click",$.scrollify.move);
+    }
+  });
+});
